e18f41c15e214bd86908fdbac168d38c32c98f04
DUNIAHAMADA5 <duniahamada2004@gmail.com>
2026-05-30 01:26:30 +0300

    Add project files.

---
+++ AudioCompressionProject/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿
+namespace AudioCompressionProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblFileSize = new System.Windows.Forms.Label();
+            this.lblDuration = new System.Windows.Forms.Label();
+            this.lblSamplingRate = new System.Windows.Forms.Label();
+            this.lblChannels = new System.Windows.Forms.Label();
+            this.lblBitRate = new System.Windows.Forms.Label();
+            this.lblCodec = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(2, 181);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(112, 48);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(2, 416);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(524, 22);
+            this.txtFilePath.TabIndex = 1;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(2, 248);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(112, 33);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(2, 299);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(111, 32);
+            this.btnPause.TabIndex = 3;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(2, 347);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(111, 28);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblCodec);
+            this.groupBox1.Controls.Add(this.lblBitRate);
+            this.groupBox1.Controls.Add(this.lblChannels);
+            this.groupBox1.Controls.Add(this.lblSamplingRate);
+            this.groupBox1.Controls.Add(this.lblDuration);
+            this.groupBox1.Controls.Add(this.lblFileSize);
+            this.groupBox1.Location = new System.Drawing.Point(120, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(493, 363);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Audio file properties";
+            // 
+            // lblFileSize
+            // 
+            this.lblFileSize.AutoSize = true;
+            this.lblFileSize.Location = new System.Drawing.Point(42, 56);
+            this.lblFileSize.Name = "lblFileSize";
+            this.lblFileSize.Size = new System.Drawing.Size(57, 17);
+            this.lblFileSize.TabIndex = 0;
+            this.lblFileSize.Text = "FileSize";
+            this.lblFileSize.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblDuration
+            // 
+            this.lblDuration.AutoSize = true;
+            this.lblDuration.Location = new System.Drawing.Point(320, 56);
+            this.lblDuration.Name = "lblDuration";
+            this.lblDuration.Size = new System.Drawing.Size(65, 17);
+            this.lblDuration.TabIndex = 1;
+            this.lblDuration.Text = "lDuration";
+            // 
+            // lblSamplingRate
+            // 
+            this.lblSamplingRate.AutoSize = true;
+            this.lblSamplingRate.Location = new System.Drawing.Point(320, 155);
+            this.lblSamplingRate.Name = "lblSamplingRate";
+            this.lblSamplingRate.Size = new System.Drawing.Size(100, 17);
+            this.lblSamplingRate.TabIndex = 2;
+            this.lblSamplingRate.Text = "Sampling Rate";
+            // 
+            // lblChannels
+            // 
+            this.lblChannels.AutoSize = true;
+            this.lblChannels.Location = new System.Drawing.Point(42, 155);
+            this.lblChannels.Name = "lblChannels";
+            this.lblChannels.Size = new System.Drawing.Size(67, 17);
+            this.lblChannels.TabIndex = 3;
+            this.lblChannels.Text = "Channels";
+            // 
+            // lblBitRate
+            // 
+            this.lblBitRate.AutoSize = true;
+            this.lblBitRate.Location = new System.Drawing.Point(342, 287);
+            this.lblBitRate.Name = "lblBitRate";
+            this.lblBitRate.Size = new System.Drawing.Size(54, 17);
+            this.lblBitRate.TabIndex = 4;
+            this.lblBitRate.Text = "BitRate";
+            // 
+            // lblCodec
+            // 
+            this.lblCodec.AutoSize = true;
+            this.lblCodec.Location = new System.Drawing.Point(53, 287);
+            this.lblCodec.Name = "lblCodec";
+            this.lblCodec.Size = new System.Drawing.Size(48, 17);
+            this.lblCodec.TabIndex = 5;
+            this.lblCodec.Text = "Codec";
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.txtFilePath);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblFileSize;
+        private System.Windows.Forms.Label lblCodec;
+        private System.Windows.Forms.Label lblBitRate;
+        private System.Windows.Forms.Label lblChannels;
+        private System.Windows.Forms.Label lblSamplingRate;
+        private System.Windows.Forms.Label lblDuration;
+    }
+}
+
---
+++ AudioCompressionProject/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using NAudio.Wave; 
+
+namespace AudioCompressionProject
+{
+    public partial class Form1 : Form
+    {
+        private WaveOutEvent outputDevice;
+        private AudioFileReader audioFile;
+        public Form1()
+        {
+            InitializeComponent();
+            txtFilePath.ReadOnly = true;
+        }
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            outputDevice?.Dispose();
+            audioFile?.Dispose();
+            base.OnFormClosing(e);
+        }
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter ="Audio Files (*.wav;*.mp3;*.aac;*.flac)|*.wav;*.mp3;*.aac;*.flac";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtFilePath.Text = openFileDialog.FileName;
+                DisplayAudioProperties(openFileDialog.FileName);
+            }
+        }
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (files.Length > 0)
+            {
+                string filePath = files[0];
+                txtFilePath.Text = filePath;
+
+                DisplayAudioProperties(filePath);
+            }
+        }
+        private void DisplayAudioProperties(string filePath)
+        {
+            try
+            {
+                FileInfo fileInfo = new FileInfo(filePath);
+                double fileSizeInMB = (double)fileInfo.Length / (1024 * 1024);
+                lblFileSize.Text = $"File Size: {fileSizeInMB:F2} MB";
+
+                using (var audioReader = new AudioFileReader(filePath))
+                {
+                    TimeSpan duration = audioReader.TotalTime;
+                    lblDuration.Text = $"Duration: {duration:hh\\:mm\\:ss}";
+
+                    lblSamplingRate.Text = $"Sampling Rate: {audioReader.WaveFormat.SampleRate} Hz";
+
+                    int channels = audioReader.WaveFormat.Channels;
+                    string channelType = (channels == 2) ? "Stereo" : (channels == 1) ? "Mono" : $"{channels} Channels";
+                    lblChannels.Text = $"Channels: {channelType}";
+
+                    lblBitRate.Text = $"Bit Rate: {audioReader.WaveFormat.AverageBytesPerSecond * 8 / 1000} kbps";
+
+                    lblCodec.Text = $"Audio Codec: {audioReader.WaveFormat.Encoding.ToString()}";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error reading audio properties: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtFilePath.Text))
+            {
+                MessageBox.Show("Please select an audio file first!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            try
+            {
+                if (outputDevice != null)
+                {
+                    outputDevice.Dispose();
+                    outputDevice = null;
+                }
+                if (audioFile != null)
+                {
+                    audioFile.Dispose();
+                    audioFile = null;
+                }
+                outputDevice = new WaveOutEvent();
+                audioFile = new AudioFileReader(txtFilePath.Text);
+                outputDevice.Init(audioFile);
+                outputDevice.Play();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error playing audio: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            if (outputDevice != null)
+            {
+                outputDevice.Pause();
+            }
+        }
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            if (outputDevice != null)
+            {
+                outputDevice.Stop();
+                outputDevice.Dispose();
+                outputDevice = null;
+            }
+            if (audioFile != null)
+            {
+                audioFile.Dispose();
+                audioFile = null;
+            }
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
